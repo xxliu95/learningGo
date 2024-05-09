77d31ae0f34fff936e6c8ccc57b8417d4a06ffc8
Xinxin Liu A <xinxin.a.liu@ericsson.com>
2024-05-09 12:26:40 +0200

    Hello World
    Change-Id: Ied86cc461714dea9f6e9d54b2d6f0af19903d19c

---
+++ HelloWorld/HelloWorld.go
@@ -0,0 +1,9 @@
+package HelloWorld
+
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Println("你好，世界！")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+// Hello world
+package main
+
+import (
+	_ "github.com/xxliu95/learningGo/HelloWorld"
+)
+
+func main() {
+	// fmt.Println("你好，世界！")
+}
